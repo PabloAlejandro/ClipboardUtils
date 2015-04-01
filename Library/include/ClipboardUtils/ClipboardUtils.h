e7b45957a1abeb57ab3f1d22e307ab943c1241d1
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2015-04-01 15:59:06 +0100

    Library updated

--- Library/include/ClipboardUtils/ClipboardUtils.h
+++ Library/include/ClipboardUtils/ClipboardUtils.h
@@ -2,21 +2,50 @@
 //  ClipboardUtils.h
 //  ClipboardUtils
 //
-//  Created by Pau on 01/04/2015.
+//  Created by PabloAlejandro on 01/04/2015.
 //  Copyright (c) 2015 PabloAlejandro. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ClipboardUtils : NSObject
 
 /**
- * copyToClipboard:
+ * copyStringToClipboard:
  *
  * Discussion:
  *   It copies a string to your device's clipboard
  *   Completion block returns whether it is completed or not
  */
-+ (void)copyToClipboard:(NSString *)string completion:(void(^)(BOOL completed))block;
++ (void)copyStringToClipboard:(NSString *)string completion:(void(^)(BOOL completed))block;
+
+
+/**
+ * copyUrlToClipboard:
+ *
+ * Discussion:
+ *   It copies a url to your device's clipboard
+ *   Completion block returns whether it is completed or not
+ */
++ (void)copyUrlToClipboard:(NSURL *)url completion:(void(^)(BOOL completed))block;
+
+/**
+ * copyImageToClipboard:
+ *
+ * Discussion:
+ *   It copies an image to your device's clipboard
+ *   Completion block returns whether it is completed or not
+ */
++ (void)copyImageToClipboard:(UIImage *)image completion:(void(^)(BOOL completed))block;
+
+/**
+ * copyToClipboard:
+ *
+ * Discussion:
+ *   It copies a color to your device's clipboard
+ *   Completion block returns whether it is completed or not
+ */
++ (void)copyColorToClipboard:(UIColor *)color completion:(void(^)(BOOL completed))block;
 
 @end
