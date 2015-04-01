d0903f314cf3a6b9ca21befde286aa7d4deb8306
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2015-04-01 15:58:35 +0100

    New instance methods

--- ClipboardUtils/ClipboardUtils/ClipboardUtils.m
+++ ClipboardUtils/ClipboardUtils/ClipboardUtils.m
@@ -2,19 +2,42 @@
 //  ClipboardUtils.m
 //  ClipboardUtils
 //
-//  Created by Pau on 01/04/2015.
+//  Created by PabloAlejandro on 01/04/2015.
 //  Copyright (c) 2015 PabloAlejandro. All rights reserved.
 //
 
 #import "ClipboardUtils.h"
-#import <UIKit/UIKit.h>
 
 @implementation ClipboardUtils
 
-+ (void)copyToClipboard:(NSString *)string completion:(void(^)(BOOL completed))block {
++ (void)copyStringToClipboard:(NSString *)string completion:(void(^)(BOOL completed))block {
     
     UIPasteboard *pb = [UIPasteboard generalPasteboard];
     [pb setString:string];
+    
+    block(true);
+}
+
++ (void)copyUrlToClipboard:(NSURL *)url completion:(void(^)(BOOL completed))block
+{
+    UIPasteboard *pb = [UIPasteboard generalPasteboard];
+    [pb setURL:url];
+    
+    block(true);
+}
+
++ (void)copyImageToClipboard:(UIImage *)image completion:(void(^)(BOOL completed))block
+{
+    UIPasteboard *pb = [UIPasteboard generalPasteboard];
+    [pb setImage:image];
+    
+    block(true);
+}
+
++ (void)copyColorToClipboard:(UIColor *)color completion:(void(^)(BOOL completed))block
+{
+    UIPasteboard *pb = [UIPasteboard generalPasteboard];
+    [pb setColor:color];
     
     block(true);
 }
